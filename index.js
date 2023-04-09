5d46b1aa0259dffe7a2049ea8143320766c384f0
awildivan <75466291+navidliwa@users.noreply.github.com>
2023-04-08 21:44:40 -0600

    add all necessary fields for the viewing employees and fixed the updateEmployeeRole function

--- index.js
+++ index.js
@@ -23,7 +23,8 @@ function start() {
                 "Add a role",
                 "Add an employee",
                 "Update a department",
-                "Exit"
+                "Update an employee role",
+                "Exit",
             ],
         }).then(function (answer) {
             switch (answer.action) {
@@ -48,29 +49,34 @@ function start() {
                 case "Update a department":
                     updateDepartment();
                     break;
+                case "Update an employee role":
+                    updateEmployeeRole();
+                    break;
                 case "Exit":
                     connection.end();
                     break;
                 default:
+                    connection.end();
                     break;
             }
         })
-}
+};
 
 const viewDepartments = () => {
 
-    var query = "SELECT * FROM department":
+    var query = "SELECT * FROM department";
 
-        connection.query(query, function (err, res) {
-            if (err) throw (err);
-            console.table(res);
-            start();
-        });
+    connection.query(query, function (err, res) {
+        if (err) throw (err);
+        console.table(res);
+        start();
+    });
 };
 
 const viewRole = () => {
 
-    var query = "SELECT role.title, role.salary, role.id, department.name FROM role RIGHT JOIN department ON role.department_id = department.id";
+    var query =
+        "SELECT role.title, role.salary, role.id, department.name AS department FROM role RIGHT JOIN department ON role.department_id = department.id";
 
     connection.query(query, function (err, res) {
         if (err) throw (err);
@@ -82,7 +88,8 @@ const viewRole = () => {
 const viewEmployee = () => {
 
     var query =
-        "SELECT t1.first_name, t1.last_name, CONCAT(t2.first_name,' ',t2.last_name) AS manager FROM employee t1 INNER JOIN employee t2 ON t1.manager_id = t2.id";
+        "SELECT t1.id, t1.first_name, t1.last_name, role.title AS role, role.salary AS salary, department.name AS department, CONCAT(t2.first_name,' ',t2.last_name) AS manager FROM employee t1 INNER JOIN role ON t1.role_id = role.id LEFT JOIN department ON role.department_id = department.id INNER JOIN employee t2 ON t1.manager_id = t2.id";
+        // "SELECT id, first_name, last_name FROM employee";
 
     connection.query(query, function (err, res) {
         if (err) throw (err);
@@ -91,67 +98,123 @@ const viewEmployee = () => {
     });
 };
 
+const addRole = () => {
+    connection.query("SELECT * FROM department", function (err, res) {
+        if (err) throw (err);
+        inquirer.prompt([
+            {
+                name: "roleName",
+                type: "input",
+                message: "What will the new role be?"
+            },
+            {
+                name: "salary",
+                type: "input",
+                message: "What will the salary be for this position?"
+            },
+            {
+                name: "deparmentId",
+                type: "list",
+                choices: function () {
+                    return res.map((department) => ({ name: department.name, value: department.id }))
+                },
+                message: "What is the deparment id for this role?",
+            },
+        ]).then(function (answer) {
+            connection.query("INSERT INTO role SET ?", {
+                title: answer.roleName,
+                salary: answer.salary,
+                department_id: answer.deparmentId
+            }),
+                start();
+        });
+    });
+};
+
 const addEmployee = () => {
     connection.query("SELECT * FROM role", function (err, res) {
         if (err) throw (err);
         inquirer.prompt([
             {
-            name: "firstName",
-            type: "input",
-            message: "first name?",
+                name: "firstName",
+                type: "input",
+                message: "first name?",
             },
             {
-            name: "lastName",
-            type: "input",
-            message: "last name?",
+                name: "lastName",
+                type: "input",
+                message: "last name?",
             },
             {
-            name: "managerId",
-            type: "input",
-            message: "first name?",
+                name: "managerId",
+                type: "list",
+                choices: function () {
+                    return new Promise(function (resolve, reject) {
+                        connection.query("SELECT * FROM employee", function (err, res) {
+                            if (err) {
+                                reject(err)
+                            } else {
+                                resolve(res.map((employee) => ({ name: employee.first_name, value: employee.id })))
+                            }
+                        });
+                    });
+                },
+                message: "Who will be this employee's manager?",
             },
             {
-            name: "addRole",
-            type: "list",
-            choices: function() {
-                return res.map((role) => ({name: role.title, value: role.id}))
+                name: "addRole",
+                type: "list",
+                choices: function () {
+                    return res.map((role) => ({ name: role.title, value: role.id }))
+                },
+                message: "role?",
             },
-            message: "role?",
-            },
-        ]).then( function (answer) {
+        ]).then(function (answer) {
             connection.query("INSERT INTO employee SET ?", {
                 first_name: answer.firstName,
                 last_name: answer.lastName,
                 manager_id: answer.managerId,
                 role_id: answer.addRole,
             }),
-            start();
+                start();
         });
     });
 };
 
 const updateEmployeeRole = () => {
     connection.query("SELECT * FROM role", function (err, res) {
+        if (err) throw err;
         inquirer.prompt([
             {
-                name: "employeeId",
-                type: "input",
-                message: "employee id?"
+                name: "employee",
+                type: "list",
+                choices: function () {
+                    return new Promise(function (resolve, reject) {
+                        connection.query("SELECT * FROM employee", function (err, res) {
+                            if (err) {
+                                reject(err)
+                            } else {
+                                resolve(res.map((employee) => ({ name: employee.first_name, value: employee.id })))
+                            }
+                        });
+                    });
+                },
+                message: "Wich employee will you be updating??"
             },
             {
                 name: "updatedRole",
                 type: "list",
                 choices: function () {
-                    return res.map((role) => ({name: role.title, value: role.id}));
+                    return res.map((role) => ({ name: role.title, value: role.id }));
                 },
-                message: "role?"
+                message: "What will be the updated role for the employee?"
             }
         ]).then(function (answer) {
-            console.log(answer.updateRole);
+            console.log(answer.updatedRole);
             connection.query(
                 "UPDATE employee SET ? WHERE ?", [
-                    {role_id: answer.updatedRole}, {id: answer.employeeId}
-                ]);
+                { role_id: answer.updatedRole }, { id: answer.employee }
+            ]);
             start();
         });
     });
--- seeds.sql
+++ seeds.sql
@@ -1,14 +1,15 @@
-USE employees_db:
+USE employees_db
 
 INSERT INTO department (name) VALUES
 ("Sales"),
 ("Human Resources");
 
 INSERT INTO role (title, salary, department_id) VALUES
-("Web Developer", 90000, 1);
-("Manager", 100000, 2);
+("Web Developer", 90000, 1),
+("Manager", 100000, 1),
+("HR", 100000, 2);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES
-("John", "Smith", 1, 2);
-("Quincy", "Adams", 2, 3);
-("Tracy", "Chapman", 1, 2)
+("John", "Smith", 1, 1),
+("Quincy", "Adams", 2, 1),
+("Tracy", "Chapman", 1, 1);
